9a460a84f6837cf438d0031b4ee222148314b338
Mika Turner <69220717+mikaturner@users.noreply.github.com>
2023-01-09 01:02:03 +0000

    Simplified guess letter checking

--- main.py
+++ main.py
@@ -2,22 +2,18 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = random.choice(word_list)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-
-chosen_word = random.choice(word_list)
-
 guess = (input("Guess a letter: ")).lower()
 
-i = 0
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
 for letter in chosen_word:
-  if guess == chosen_word[i]:
+  if letter == guess:
     print("Right")
-    i += 1
   else: 
     print("Wrong")
-    i += 1
 
