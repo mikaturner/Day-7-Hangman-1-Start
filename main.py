732cffe749191a8d8fb2f70a8c095ac9ecc9c2f2
Mika Turner <69220717+mikaturner@users.noreply.github.com>
2023-01-09 00:57:48 +0000

    Implemented choosing random word, user letter guess input, and checking of letter against word

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Step 1 
-
+import random
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
@@ -7,3 +7,17 @@ word_list = ["aardvark", "baboon", "camel"]
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+chosen_word = random.choice(word_list)
+
+guess = (input("Guess a letter: ")).lower()
+
+i = 0
+for letter in chosen_word:
+  if guess == chosen_word[i]:
+    print("Right")
+    i += 1
+  else: 
+    print("Wrong")
+    i += 1
+
